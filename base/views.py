918d51a34e1fce51ef4ecf2753fed8af29ada2c1
张玉坡 <zhangyupo@yunjiazheng.com>
2016-06-15 19:18:17 +0800

    修改用户方法

--- base/views.py
+++ base/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render,render_to_response
 from django.http import HttpResponse,HttpResponseRedirect
 from django.contrib import auth
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
 
+@login_required(login_url='/users/user_login/')
+def home(request,name):
+    name = request.name
+    return render_to_response('base/home.html',{'name':name})
 
-def home(request):
-    return render_to_response('base/home.html')
 
-
-
+@login_required(login_url='/users/user_login/')
 def host_list(request):
     return render_to_response('base/host_list.html')
--- users/views.py
+++ users/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render,render_to_response
 from django.http import HttpResponse,HttpResponseRedirect
-from django.contrib import auth
+from django.contrib import auth,sessions
 from django.contrib.auth.models import User ,UserManager
 
 #登录
-def login(request):
+def user_login(request):
     if request.method != 'POST':
         return render_to_response('users/login.html')
     else:
@@ -17,18 +17,18 @@ def login(request):
         user = auth.authenticate(username=username,password=password)
         if user is not None and user.is_active:
             auth.login(request,user)
-            return HttpResponseRedirect('base/home')
+            lastname = request.session['last_name']
+            return HttpResponseRedirect('base/home',lastname)
         else:
             error.append('账号或者密码错误')
             return render_to_response('users/login.html',{'error':error})
 
 
 
-# @login_required(login_url='/book/login/')
-# def logout(request):
-#     auth.logout(request)
-#     return HttpResponseRedirect("/book/login/")
-#     # return render_to_response('login.html')
+#@login_required(login_url='/book/login/')
+def user_logout(request):
+    auth.logout(request)
+    return HttpResponseRedirect("login")
 
 
 
@@ -36,6 +36,8 @@ def login(request):
 def user_list(request):
     user_info = User.objects.all()
     return render_to_response('users/user_list.html',{'user_info':user_info})
+
+
 
 def add_user(request):
     UserManager.create_user()
